c15f6832952f2bd683e4f90eac1f1ee9f57f3c23
claudiopatti <claudiopatti1997@gmail.com>
2024-09-27 14:46:09 +0200

    agiunta tabella con bootstrap e messo all'interno i risultati

--- index.php
+++ index.php
@@ -49,14 +49,46 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <title>PHP Hotel</title>
     </head>
     <body>
         <div>
-            <?php foreach($hotels as $hotel) {
+        
+            <div class="container" >
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nome Hotel</th>
+                        <th scope="col">Descrizone Hotel</th>
+                        <th scope="col">Parcheggio Hotel</th>
+                        <th scope="col">Voto Hotel</th>
+                        <th scope="col">Distanza Hotel dal centro</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php for ($i=0; $i < count($hotels) ; $i++) { 
+                            
+                                ?>
+                        <tr>
+                            <th  scope="row"> <?php echo ($i + 1 )?> </th>
+                            <td> <?php echo $hotels[$i]['name'] ?> </td>
+                            <td> <?php echo $hotels[$i]['description'] ?> </td>
+                            <td> <?php echo $hotels[$i]['parking'] ?> </td>
+                            <td> <?php echo $hotels[$i]['vote'] ?> </td>
+                            <td> <?php echo $hotels[$i]['distance_to_center'] ?> </td>
+                        </tr>
+                        <?php
+                        };
+                        ?>
+                    </tbody>
+                </table>
 
-            ?>
-
+            </div>
+<!-- 
             <div>
                 <ul>
                     <p>
@@ -77,11 +109,11 @@
                 </ul>
                 
 
-            </div>
+            </div> -->
 
-            <?php 
-            }
-            ?>
         </div>
+
+        <!-- bootstrap js  -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </body>
 </html>
